4239e2b3745dbba80c365ce789f483c15c8405fd
AntiqueR <antique.pendulum@gmail.com>
2019-01-29 18:18:39 +0900

    First Commit

---
+++ HelloSpirit/Grass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using AngleSharp.Html.Parser;
+using System.IO;
+using AngleSharp.Html;
+using System.Diagnostics;
+using Microsoft.Toolkit.Wpf.UI.Controls;
+
+namespace HelloTask
+{
+    internal static class Grass
+    {
+        private const string style = "<style> text { fill: white; font-family: 'Quicksand', sans-serif; font-size: 12px; line-height: 1.5; } </style>";
+
+        internal static void GetGrass(WebView view)
+        {
+            var client = new HttpClient();
+            var a = client.GetAsync($"https://github.com/users/{App.GitHubName}/contributions").Result;
+            var html = a.Content.ReadAsStringAsync().Result;
+
+            var parser = new HtmlParser();
+            var sw = new StringWriter();
+            var formatter = new PrettyMarkupFormatter();
+            var content = parser.ParseDocument(html).GetElementsByTagName("svg").First().OuterHtml;
+            var htmlStr = $"<html> <link href=\"https://fonts.googleapis.com/css?family=Quicksand\" rel=\"stylesheet\"> {style} <body bgcolor=\"black\"> {content} </body> </html>";
+            view.NavigateToString(htmlStr);
+        }
+    }
+}
