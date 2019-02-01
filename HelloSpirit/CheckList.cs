c7579e98d2fdb258c938db5bd9fcd853e246bedf
AntiqueR <antique.pendulum@gmail.com>
2019-02-01 22:37:49 +0900

    add 2 classes (Spirit, CheckList)
    delete "textBox's  function"

---
+++ HelloSpirit/CheckList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloSpirit
+{
+    public class CheckList
+    {
+        public string Title { get; set; }
+        public bool IsFinished { get; set; }
+    }
+}
---
+++ HelloSpirit/Spirit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloSpirit
+{
+    public class Spirit
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime LimitDate { get; set; }
+        public List<CheckList> CheckLists { get; set; }
+    }
+}
