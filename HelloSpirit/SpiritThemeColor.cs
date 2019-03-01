97f0ec0060831beef67b7cc236fa9af6b319bdaf
AntiqueR <antique.pendulum@gmail.com>
2019-03-01 23:28:23 +0900

    add ThemeColors, add Application Icon

---
+++ HelloSpirit/SpiritThemeColor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace HelloSpirit
+{
+    public static class SpiritThemeColor
+    {
+        #region Window
+        public static SolidColorBrush WindowBackGround { get; set; }
+
+        public static SolidColorBrush TitleFontColor{ get; set; }
+
+        public static SolidColorBrush MinButtonBackground { get; set; }
+        public static SolidColorBrush MinButtonForeground { get; set; }
+        public static SolidColorBrush MinButtonMouseOverBackground { get; set; }
+
+        public static SolidColorBrush CloseButtonBackground { get; set; }
+        public static SolidColorBrush CloseButtonForeground { get; set; }
+        public static SolidColorBrush CloseButtonMouseOverBackground { get; set; }
+        #endregion
+
+        #region MainWindow
+        public static SolidColorBrush HelloTextColor { get; set; }
+
+        public static SolidColorBrush ListBackground { get; set; }
+        public static SolidColorBrush ListTitleColor { get; set; }
+
+        public static SolidColorBrush ListDeleteButtonDefaultBackground { get; set; }
+        public static SolidColorBrush ListDeleteButtonMouseOverBackground { get; set; }
+
+        public static SolidColorBrush ListPlusButtonBackground { get; set; }
+        public static SolidColorBrush ListPlusButtonForeground { get; set; }
+        public static SolidColorBrush ListPlusButtonMouseOverBackground { get; set; }
+
+        public static SolidColorBrush SpiritAddButtonBackground { get; set; }
+        public static SolidColorBrush SpiritAddButtonForeground { get; set; }
+        public static SolidColorBrush SpiritAddButtonMouseOverBackground { get; set; }
+
+        public static SolidColorBrush FootterColor { get; set; }
+        #endregion
+
+        #region SpiritWindow
+        public static SolidColorBrush SpiritTitleForeground { get; set; }
+        public static SolidColorBrush SpiritTitleBorderColor { get; set; }
+
+        public static SolidColorBrush SpiritDescriptionNormalBackground { get; set; }
+        public static SolidColorBrush SpiritDescriptionNormalForeground { get; set; }
+        public static SolidColorBrush SpiritDescriptionActiveBackground { get; set; }
+        public static SolidColorBrush SpiritDescriptionActiveForeground { get; set; }
+        public static SolidColorBrush SpiritDecriptionBorderColor { get; set; }
+
+        public static SolidColorBrush CheckListAddButtonBackground { get; set; }
+        public static SolidColorBrush CheckListAddButtonForeground { get; set; }
+        public static SolidColorBrush CheckListAddButtonMouseOverBackground { get; set; }
+
+        public static SolidColorBrush CheckListBackground { get; set; }
+        public static SolidColorBrush CheckListForeground { get; set; }
+        public static SolidColorBrush CheckListMouseOverBackground { get; set; }
+
+        public static SolidColorBrush CheckBoxBackground { get; set; }
+        public static SolidColorBrush CheckBoxForeground { get; set; }
+
+        public static SolidColorBrush NumberFontColor { get; set; }
+
+        public static SolidColorBrush DeleteButtonBackground { get; set; }
+        public static SolidColorBrush DeleteButtonForeground { get; set; }
+        public static SolidColorBrush DeleteButtonMouseOverBackground { get; set; }
+        #endregion
+    }
+}
